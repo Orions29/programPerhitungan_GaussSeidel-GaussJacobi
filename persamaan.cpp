0e502ff0c700b9a50df58ed19c29ab915e38e43c
Rio Meidi Ataurrahman <riomeidi29@gmail.com>
2025-09-27 17:05:47 +0700

    Feat - Penambahan feature pengecekan konvergensi dan dominan diagonal

--- persamaan.cpp
+++ persamaan.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file persamaan.cpp
+ * @author Rio Meidi Ataurrahman (riomeidi29@gmail.com)
+ * @brief Untuk Tugas Komputer Dan Numerasi ke 1
+ * @version 0.1
+ * @date 2025-09-27
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
 #include <iostream>
 // Array Yang Dinamis
 #include <vector>
@@ -63,7 +73,7 @@ void gaussSeidelTemplate(const vector<vector<double>> &persIn, vector<double> &h
  *  @param mode - `0` or`False`berhenti di error_value tertentu
  *              - `1` or `True` berhenti di iterasi
  */
-void gaussMenu(vector<vector<double>> &persIn, vector<double> hasil, short *jmlpers, short *var, bool menu)
+void gaussMenu(const vector<vector<double>> &persIn, vector<double> hasil, const short *jmlpers, const short *var, const bool menu)
 {
     // Membersihkan Seluruh Program Sebelumnya
     system("cls");
@@ -134,180 +144,251 @@ void gaussMenu(vector<vector<double>> &persIn, vector<double> hasil, short *jmlp
     }
 }
 
-void mainMenu(vector<vector<double>> &persIn, vector<double> &hasil)
+/**
+ * @brief Mengecek Konvergensi Diagonal dan Mengecek Dominansi Diagonal
+ *
+ * @param persIn Koefisien persamaan yang mau dicek
+ * @param jmlPers Jumlah Persamaan
+ * @param var Jumlah Variabel Tiap Persamaan
+ */
+void checkAndSwapKonvergenDominan(vector<vector<double>> &persIn, const short jmlPers, const short var, string &log)
 {
-    short var, jmlPers, menu;
-    cout << "\033[44;1;37m<==== Menu Utama Pendekatan Numerik ====>\033[0m" << "\n  => Rio Meidi A / 123240175" << endl
-         << endl;
-    // Menanyakan mau Berapa Variabel
-    while (true)
+    for (int i = 0; i < jmlPers; ++i)
     {
-        cout << "\n>> Mau Pakai Berapa Variabel?: ";
-        cin >> var;
-        if (cin.fail())
+        double maxKoef = 0.0;
+        int maxInRow = i;
+        // Akan Memulai Perulangan dimulai dengan baris selanjutnya, Baris Sebelumnya yang sudah dicek akan dibiarkan
+        for (int k = i; k < jmlPers; ++k)
         {
-            cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
-
-            cin.clear();
-            cin.ignore(1000, '\n');
+            if (abs(persIn[k][i]) > maxKoef)
+            {
+                maxKoef = abs(persIn[k][i]);
+                maxInRow = k;
+            }
+        }
+        if (maxInRow != i)
+        {
+            swap(persIn[i], persIn[maxInRow]);
+            cout << "\033[1;33m Baris " << maxInRow + 1 << " ditukar dengan Baris " << i + 1
+                 << " untuk Dominasi Diagonal!\033[0m" << endl;
+        }
+        // Ngecek apakah persamaan ini dominan atau enggak
+        double sumNonDiagonal = 0.0;
+        for (int j = 0; j < var - 1; ++j)
+        {
+            if (i != j)
+            {
+                sumNonDiagonal += abs(persIn[i][j]);
+            }
+        }
+        if (abs(persIn[i][i]) > sumNonDiagonal)
+        {
+            log += "\033[1;32m >> Persamaan " + to_string(i + 1) + " Sudah Dominan Diagonal.\033[0m\n";
         }
         else
         {
-            var++;
-            break;
-        }
-        // Menambahkan Var untuk Konstanta Z
-    }
-    // Menanyakan Mau berapa Persamaan
-    while (true)
-    {
-        cout << "\n>> Mau Pakai Berapa Pers?: ";
-        cin >> jmlPers;
-
-        if (cin.fail())
-        {
-            cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
-
-            cin.clear();
-            cin.ignore(1000, '\n');
-        }
-        else
-        {
-            break;
-        }
-    }
-    // Mereserve Memory untuk sejumlah persamaan dan variabelnya
-    persIn.resize(jmlPers);
-    hasil.resize(var);
-    // Resize Tiap Dimensi dari Array Persamaan
-    for (int i = 0; i < jmlPers; i++)
-    {
-        persIn[i].resize(var);
-    }
-    // Memasukkan Persamaan
-    for (int i = 0; i < jmlPers; i++)
-    {
-        cout << "\n >> Masukkan Persamaan ke - " << i + 1 << endl;
-        for (int j = 0; j < var; j++)
-        {
-            while (true)
-            {
-                if (j == (var - 1))
-                {
-                    cout << "\n\t> Hasil Z : ";
-                    cin >> persIn[i][j];
-                }
-                else
-                {
-                    cout << "\n\t> Konstanta X" << j + 1 << " : ";
-                    cin >> persIn[i][j];
-                }
-                if (cin.fail())
-                {
-                    cout << "\033[1;31mKonstanta Tidak Valid!\033[0m";
-                    cin.clear();
-                    cin.ignore(1000, '\n');
-                    continue;
-                }
-                break;
-            }
-        }
-    }
-    cout << ">> Kondisi Awal Var?: " << endl;
-    // Var - 1 karena hasil temp persamaan ditaruh di belakang sendiri
-    for (int i = 0; i < var - 1; i++)
-    {
-        while (true)
-        {
-            cout << "X" << i + 1 << " : ";
-            cin >> hasil[i];
-
-            if (cin.fail())
-            {
-                cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
-
-                cin.clear();
-                cin.ignore(1000, '\n');
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
-    // Loop Menu
-    while (true)
-    {
-        cout << "Persamaan yang Mau dihitung ada [" << jmlPers << "] Dengan masing-masing memiliki [" << var - 1 << "] variabel" << endl;
-        cout.unsetf(ios::fixed);
-        for (int i = 0; i < jmlPers; i++)
-        {
-            cout << "Persamaan (" << i + 1 << ") ";
-            for (int j = 0; j < var; j++)
-            {
-                // Menampilkan semua persamaan kecuali nilai Z
-                if (j < var - 1)
-                {
-                    // Depan sendiri gaperlu dikasi penanda +
-                    if (j > 0)
-                    {
-                        if (persIn[i][j] >= 0)
-                        {
-                            cout << "+" << persIn[i][j] << "X" << j + 1 << " ";
-                            continue;
-                        }
-                        // karena kalau dia minus langsung ada minusnya di terminal
-                    }
-                    // Kalau dia depan sendiri tampilkan apa adanya
-                    cout << persIn[i][j] << "X" << j + 1 << " ";
-                    continue;
-                }
-                cout << "= " << persIn[i][j];
-            }
-            cout << endl;
-        }
-
-        cout << "Pilih Mau Pakai Metode apa?: \n";
-        cout << "\t1.) Gauss-Jacobi\n\t2.) Gauss-Seidel\n\t3.) Exit" << endl;
-        while (true)
-        {
-            cout << "\n>> Menu Keberapa: ";
-            cin >> menu;
-
-            if (cin.fail())
-            {
-                cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
-                cin.clear();
-                cin.ignore(1000, '\n');
-            }
-            else if (menu > 3)
-            {
-                cout << "\033[1;31mOut Of Index!\033[0m";
-                cin.clear();
-                cin.ignore(1000, '\n');
-            }
-            else
-            {
-                break;
-            }
-        }
-        switch (menu)
-        {
-        case 1:
-            gaussMenu(persIn, hasil, &jmlPers, &var, 0);
-            break;
-        case 2:
-            gaussMenu(persIn, hasil, &jmlPers, &var, 1);
-            break;
-        case 3:
-            exit(0);
-            break;
-        default:
-            break;
+            log += "\033[1;5;33m >> Persamaan " + to_string(i + 1) + "\033[0m ,Tidak Strictly Dominan Diagonal (Mungkin jadi nih ye tidak konvergen).\n";
         }
     }
 }
 
+/**
+ * @brief Untuk Menampilkan Menu dan Looping Main Menu
+ *
+ * @param persIn Persamaan yang mau diselesaikan
+ * @param hasil Hasil dari persamaan nantinya
+ */
+void mainMenu(vector<vector<double>> &persIn, vector<double> &hasil)
+{
+    bool loopMain = false, loopSubMenu = true;
+    do
+    {
+        system("cls");
+        loopSubMenu = true;
+        short var, jmlPers, menu;
+        string logErr;
+        cout << "\033[44;1;37m<==== Menu Utama Pendekatan Numerik ====>\033[0m" << "\n  => Rio Meidi A / 123240175" << endl
+             << endl;
+        // Menanyakan mau Berapa Variabel
+        while (true)
+        {
+            cout << "\n>> Mau Pakai Berapa Variabel?: ";
+            cin >> var;
+            if (cin.fail())
+            {
+                cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
+
+                cin.clear();
+                cin.ignore(1000, '\n');
+            }
+            else
+            {
+                var++;
+                break;
+            }
+            // Menambahkan Var untuk Konstanta Z
+        }
+        // Menanyakan Mau berapa Persamaan
+        while (true)
+        {
+            cout << "\n>> Mau Pakai Berapa Pers?: ";
+            cin >> jmlPers;
+
+            if (cin.fail())
+            {
+                cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
+
+                cin.clear();
+                cin.ignore(1000, '\n');
+            }
+            else
+            {
+                break;
+            }
+        }
+        // Mereserve Memory untuk sejumlah persamaan dan variabelnya
+        persIn.resize(jmlPers);
+        hasil.resize(var);
+        // Resize Tiap Dimensi dari Array Persamaan
+        for (int i = 0; i < jmlPers; i++)
+        {
+            persIn[i].resize(var);
+        }
+        // Memasukkan Persamaan
+        for (int i = 0; i < jmlPers; i++)
+        {
+            cout << "\n >> Masukkan Persamaan ke - " << i + 1 << endl;
+            for (int j = 0; j < var; j++)
+            {
+                while (true)
+                {
+                    if (j == (var - 1))
+                    {
+                        cout << "\n\t> Hasil Z : ";
+                        cin >> persIn[i][j];
+                    }
+                    else
+                    {
+                        cout << "\n\t> Konstanta X" << j + 1 << " : ";
+                        cin >> persIn[i][j];
+                    }
+                    if (cin.fail())
+                    {
+                        cout << "\033[1;31mKonstanta Tidak Valid!\033[0m";
+                        cin.clear();
+                        cin.ignore(1000, '\n');
+                        continue;
+                    }
+                    break;
+                }
+            }
+        }
+        cout << ">> Kondisi Awal Var?: " << endl;
+        // Var - 1 karena hasil temp persamaan ditaruh di belakang sendiri
+        for (int i = 0; i < var - 1; i++)
+        {
+            while (true)
+            {
+                cout << "X" << i + 1 << " : ";
+                cin >> hasil[i];
+
+                if (cin.fail())
+                {
+                    cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
+
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        // Cek apakah dia konvergen atau tidak
+        checkAndSwapKonvergenDominan(persIn, jmlPers, var, logErr);
+        // Loop Menu
+        do
+        {
+            cout << logErr;
+            cout << "Persamaan yang Mau dihitung ada [" << jmlPers << "] Dengan masing-masing memiliki [" << var - 1 << "] variabel" << endl;
+            cout.unsetf(ios::fixed);
+            for (int i = 0; i < jmlPers; i++)
+            {
+                cout << "Persamaan (" << i + 1 << ") ";
+                for (int j = 0; j < var; j++)
+                {
+                    // Menampilkan semua persamaan kecuali nilai Z
+                    if (j < var - 1)
+                    {
+                        // Depan sendiri gaperlu dikasi penanda +
+                        if (j > 0)
+                        {
+                            if (persIn[i][j] >= 0)
+                            {
+                                cout << "+" << persIn[i][j] << "X" << j + 1 << " ";
+                                continue;
+                            }
+                            // karena kalau dia minus langsung ada minusnya di terminal
+                        }
+                        // Kalau dia depan sendiri tampilkan apa adanya
+                        cout << persIn[i][j] << "X" << j + 1 << " ";
+                        continue;
+                    }
+                    cout << "= " << persIn[i][j];
+                }
+                cout << endl;
+            }
+
+            cout << "Pilih Mau Pakai Metode apa?: \n";
+            cout << "\t1.) Gauss-Jacobi\n\t2.) Gauss-Seidel\n\t3.) Persamaan Baru\n\t4.) Exit" << endl;
+            while (true)
+            {
+                cout << "\n>> Menu Keberapa: ";
+                cin >> menu;
+
+                if (cin.fail())
+                {
+                    cout << "\033[1;31mMasukan Tidak Valid!\033[0m";
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                }
+                else if (menu > 4)
+                {
+                    cout << "\033[1;31mOut Of Index!\033[0m";
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
+            switch (menu)
+            {
+            case 1:
+                gaussMenu(persIn, hasil, &jmlPers, &var, 0);
+                break;
+            case 2:
+                gaussMenu(persIn, hasil, &jmlPers, &var, 1);
+                break;
+            case 3:
+                loopSubMenu = false;
+                loopMain = true;
+                break;
+            case 4:
+                loopSubMenu = false;
+                loopMain = false;
+                break;
+            default:
+                // barangkali i think..
+                cout << "\033[1;5;31mPlis ini Error handling terakhir....\033[0m" << endl;
+                system("pause");
+                break;
+            }
+        } while (loopSubMenu);
+    } while (loopMain);
+}
 int main(int argc, char const *argv[])
 {
     // Inisialisasi Tempat Untuk Persamaan dan Hasil
